84354cc2901b40800efc1ed54ac332b2c6db9b91
Jia <vernalin1422@gmail.com>
2023-01-08 14:41:04 +0800

    OK

--- GachaMachine/Form1.cs
+++ GachaMachine/Form1.cs
@@ -19,13 +19,13 @@ namespace GachaMachine
 		int sum; //總金額
 		int ship = 50; //商品金額
 		/// <summary>
-		/// 扭蛋機程式 爛透了版本
+		/// 扭蛋機程式
 		/// </summary>
 		/// <param name="sender"></param>
 		/// <param name="e"></param>
 		private void btncoin_Click(object sender, EventArgs e)
 		{
-			bool isInt = int.TryParse(txtmoney.Text, out int money);
+			bool isInt = int.TryParse(txtmoney.Text, out int money); //投入金額
 			
 			if (!isInt || money<=0)
 			{
@@ -35,82 +35,50 @@ namespace GachaMachine
 			}
 			sum += money;
 
-			txtpanel.Clear();
-			
-			if (sum >= 50)
-			{
-				btnreturnMoney.Enabled = true;
-				btnship.Enabled = true;
-				txtpanel.Text += $"State:投入足夠金額，等待客人指令就出貨\r\nmoney:{sum}";
-			}
-			else
-			{
-				btnreturnMoney.Enabled = true;
-				txtpanel.Text += $"State:有投幣，但金額還不足\r\nmoney:{sum}";
-			}
+			result();
 		}
 
 		private void btnreturnMoney_Click(object sender, EventArgs e)
 		{
-			bool isInt = int.TryParse(txtmoney.Text, out int money);
-			if (!isInt || money <= 0)
-			{
-				MessageBox.Show("請輸入正確金額!!");
-				txtmoney.Clear();
-				return;
-			}
+			sum = 0;
 
-			if (sum >= money)
-			{
-				sum -= money;
-			}
-			else 
-			{
-				MessageBox.Show("請輸入正確金額!!");
-				txtmoney.Clear();
-				return; 
-			}
-				
-			txtpanel.Clear();
-
-			if (sum >= 50)
-			{				
-				txtpanel.Text += $"State:投入足夠金額，等待客人指令就出貨\r\nmoney:{sum}";
-			}
-			else if(sum == 0)
-			{
-				btnship.Enabled = false;
-				txtpanel.Text += "State:StandbyState 機器沒在工作，等待下一個客人操作\r\nMoney: 0";
-
-			}
-			else
-			{
-				btnship.Enabled = false;
-				txtpanel.Text += $"State:有投幣，但金額還不足\r\nmoney:{sum}";
-			}
+			result();
 			MessageBox.Show("已完成退幣作業");
 		}
 
 		private void btnship_Click(object sender, EventArgs e)
 		{
-			sum -= ship;
+			sum -= ship;			
+
+			result();
+			MessageBox.Show("已出貨");
+		}
+
+		public string result()
+		{
 			txtpanel.Clear();
 			if (sum >= 50)
 			{
-				txtpanel.Text += $"State:投入足夠金額，等待客人指令就出貨\r\nmoney:{sum}";
+				btnship.Enabled = true;
+				btnreturnMoney.Enabled = true;
+
+				txtpanel.Text += $"State:投入足夠金額，等待客人指令就出貨\r\nMoney:{sum}";
 			}
 			else if (sum == 0)
 			{
 				btnship.Enabled = false;
-				txtpanel.Text += "State:StandbyState 機器沒在工作，等待下一個客人操作\r\nMoney: 0";
+				btnreturnMoney.Enabled = false;
 
+				txtpanel.Text += "State:StandbyState 機器沒在工作，等待下一個客人操作\r\nMoney: 0";
 			}
 			else
 			{
 				btnship.Enabled = false;
-				txtpanel.Text += $"State:有投幣，但金額還不足\r\nmoney:{sum}";
+				btnreturnMoney.Enabled = true;
+
+				txtpanel.Text += $"State:有投幣，但金額還不足\r\nMoney:{sum}";
 			}
-			MessageBox.Show("已出貨");
+			return txtpanel.Text;
 		}
 	}
 }
